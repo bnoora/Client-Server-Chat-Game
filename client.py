1b3294c9b2f85ba1f001431cc17947a7cf3098a4
bnoora <bnooranii@gmail.com>
2023-12-05 16:30:49 -0800

    size fix for max msg size comments pep8 fixes

--- client.py
+++ client.py
@@ -1,5 +1,13 @@
 import socket
 
+
+'''
+Name: Client
+Description: Client class that handles the connection to the server and
+sending and receiving data
+'''
+
+
 class Client:
     def __init__(self, host, port):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,18 +16,28 @@ class Client:
         self.server_turn = False
         self.client_turn = True
 
+    '''
+    Name: connect
+    Description: Connects to the server
+    additionally prints out instructions for the user
+    '''
     def connect(self):
         try:
             self.client.connect((self.host, self.port))
             print("Connected to server on {}:{}".format(self.host, self.port))
-            print("Enter message to send to server, Wait for input prompt before starting")
+            print("Enter message to send to server, "
+                  "Wait for input prompt before starting")
             print("Enter /q to quit")
-            print("Enter /play rock paper scissors or /play rps to play rock paper scissors")
-            
+            print("Enter /play rock paper scissors or /play rps "
+                  "to play rock paper scissors")
         except Exception as e:
             print("Failed to connect to server: {}".format(e))
             exit()
 
+    '''
+    Name: start
+    Description: Starts the client, and sends and receives data
+    '''
     def start(self):
         try:
             while True:
@@ -41,12 +59,25 @@ class Client:
         finally:
             self.client.close()
 
+    '''
+    Name: recvData
+    Description: Receives data from the server
+    '''
     def recvData(self):
-        return self.client.recv(1024).decode()
+        return self.client.recv(4096).decode()
 
+    '''
+    Name: sendData
+    Description: Sends data to the server
+    '''
     def sendData(self, data):
         self.client.sendall(data.encode())
 
+    '''
+    Name: handleSpacialCommand
+    Description: Handles special commands, such as /q to quit,
+    and /play rock paper scissors
+    '''
     def handleSpacialCommand(self, command):
         if command == "/q" and self.server_turn:
             print("Client disconnected")
@@ -54,16 +85,24 @@ class Client:
         elif command == "/q" and self.client_turn:
             print("Server disconnected")
             self.handleQuitCommand()
-        elif command == "/play rock paper scissors" or command == "/play rps" or command == "/play rockpaperscissors":
-            print(self.server_turn)
-            print(self.client_turn)
+        elif (command == "/play rock paper scissors" or
+              command == "/play rps" or
+              command == "/play rockpaperscissors"):
             self.handleRockPaperScissors()
             return
-        
+    '''
+    Name: handleQuitCommand
+    Description: Handles the /q command, closes the client, and exits
+    the program
+    '''
     def handleQuitCommand(self):
         self.client.close()
         exit()
 
+    '''
+    Name: handleRockPaperScissors
+    Description: Handles the rock paper scissors game
+    '''
     def handleRockPaperScissors(self):
         print("Starting Rock Paper Scissors")
         winner = None
@@ -102,7 +141,8 @@ class Client:
             elif serverChoice == "scissors" and clientChoice == "paper":
                 print("Server wins")
                 serverScore += 1
-            print("Server score: {} Client score: {}".format(serverScore, clientScore))
+            print("Server score: {} Client score: {}".format(serverScore,
+                                                             clientScore))
             if serverScore == 3:
                 winner = "Server"
             elif clientScore == 3:
@@ -110,8 +150,8 @@ class Client:
 
 
 if __name__ == "__main__":
-    host = "localhost" 
-    port = 12345  
+    host = "localhost"
+    port = 12345
     client = Client(host, port)
     client.connect()
     client.start()
--- server.py
+++ server.py
@@ -1,5 +1,13 @@
 import socket
 
+
+'''
+Name: Server
+Description: Server class that handles the connection to the client and
+sending and receiving data
+'''
+
+
 class Server:
     def __init__(self, port):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,26 +18,44 @@ class Server:
         self.server_turn = False
         self.client_turn = True
 
-
+    '''
+    Name: start
+    Description: Starts the server, listens for a connection, and accepts
+    the connection
+    additionally prints out instructions for the user
+    '''
     def start(self):
+        # Listen for 1 connection
         self.server.listen(1)
         print("Server listening on port {}".format(self.port))
-
+        # Accept connection
         self.client_socket, self.client_address = self.server.accept()
         print("Connection from {}".format(self.client_address))
-        print("Enter message to send to client, Wait for input prompt before starting")
+        print("Enter message to send to client, "
+              "Wait for input prompt before starting")
         print("Enter /q to quit")
-        print("Enter /play rock paper scissors or /play rps to play rock paper scissors")
+        print("Enter /play rock paper scissors or /play "
+              "rps to play rock paper scissors")
 
+    '''
+    Name: handle_client
+    Description: Handles the client, receives data from the client,
+    and sends data to the client
+    '''
     def handle_client(self):
         try:
             while True:
+                # If it's the client's turn
                 if self.client_turn:
+                    # Receive data from client
                     data = self.recvData()
                     if data:
+                        # Print message from client
                         print("Client: {}".format(data))
+                        # update Turns
                         self.server_turn = True
                         self.client_turn = False
+                        # check for special commands
                         self.handleSpacialCommand(data)
                 elif self.server_turn:
                     reply = input("Enter your reply: ")
@@ -43,46 +69,72 @@ class Server:
         finally:
             self.client_socket.close()
 
+    '''
+    Name: recvData
+    Description: Receives data from the client
+    '''
     def recvData(self):
-        return self.client_socket.recv(1024).decode()
-    
+        return self.client_socket.recv(4096).decode()
+
+    '''
+    Name: sendData
+    Description: Sends data to the client
+    '''
     def sendData(self, data):
         self.client_socket.sendall(data.encode())
 
+    '''
+    Name: handleSpacialCommand
+    Description: Handles special commands, such as /q to quit,
+    and /play rock paper scissors
+    '''
     def handleSpacialCommand(self, command):
+        # if /q is detected
         if command == "/q" and self.server_turn:
             print("Client disconnected")
             self.handleQuitCommand()
         elif command == "/q" and self.client_turn:
             print("Server disconnected")
-            self.handle_quit_command()
-        elif command == "/play rock paper scissors" or command == "/play rps" or command == "/play rockpaperscissors":
-            print(self.server_turn)
-            print(self.client_turn)
+            self.handleQuitCommand()
+        # if /play rock paper scissors is detected
+        elif (command == "/play rock paper scissors" or
+              command == "/play rps" or command == "/play rockpaperscissors"):
             self.handleRockPaperScissors()
             return
         else:
             return
 
+    '''
+    Name: handleQuitCommand
+    Description: Handles the /q command, closes the server, and exits
+    the program
+    '''
     def handleQuitCommand(self):
         self.client_socket.close()
         self.server.close()
         print("Server closed")
-        exit() 
+        exit()
 
-
+    '''
+    Name: handleRockPaperScissors
+    Description: Handles the rock paper scissors game, sends and receives data
+    from the client
+    and determines the winner of the game
+    '''
     def handleRockPaperScissors(self):
         print("Starting rock paper scissors")
         winner = None
         serverScore = 0
         clientScore = 0
         while winner is None:
+            # if it's the server's turn at the start of the game
             if self.server_turn:
                 serverChoice = input("Server choice: ")
                 self.sendData(serverChoice)
                 clientChoice = self.recvData()
                 self.handleSpacialCommand(clientChoice)
                 self.handleSpacialCommand(serverChoice)
+            # if it's the client's turn at the start of the game
             elif self.client_turn:
                 clientChoice = self.recvData()
                 self.handleSpacialCommand(clientChoice)
@@ -113,7 +165,8 @@ class Server:
             elif serverChoice == "scissors" and clientChoice == "paper":
                 print("Server wins")
                 serverScore += 1
-            print("Server score: {} Client score: {}".format(serverScore, clientScore))
+            print("Server score: {} Client score: {}".format(serverScore,
+                                                             clientScore))
             if serverScore == 3:
                 winner = "Server"
             elif clientScore == 3:
@@ -122,12 +175,13 @@ class Server:
             self.sendData("The winner is: {}".format(winner))
             self.client_turn = True
             self.server_turn = False
+
         elif self.client_turn:
+            # recv data to resolve turn issue
             self.recvData()
             self.sendData("The winner is: {}".format(winner))
             self.client_turn = True
             self.server_turn = False
-
 
 
 if __name__ == "__main__":
